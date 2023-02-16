bdee0dd1db6028fb9034fc13fb382eaaa4d08adf
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-16 10:56:12 -0800

    5th

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main- prints a string followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
