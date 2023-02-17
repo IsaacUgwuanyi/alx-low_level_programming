9d4c205c3ef18249957168932dbe2f122dee0c12
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-17 03:44:56 -0800

    hot2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <time.h> 
+#include <stdlib.h> 
+#include <time.h>
 /**
  * more headers goes there
  * main - the main function generates random numbers
