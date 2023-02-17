4454550973c1998444b5617ede33541f6c5731b9
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-17 07:55:36 -0800

    fixes

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,11 +11,13 @@
 int main(void)
 	{
 	char alpha[26];
-	alpha = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,	t,u,v,w,x,y,z");
+	int i;
+	alpha = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,	t,u,v,w,x,y,z")
 		
 	for (i=0;i<26;i++)
 	{
-	putchar(alpha[i]\n);
+	putchar(alpha[i]);
+	putchar("\n");
 	}
 	return 0;
 	}
