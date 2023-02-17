3444cb77997792ad258634578bc4ce7b0cd525a2
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-17 08:30:35 -0800

    capital_and_lowercase_print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,18 +2,20 @@
 /**
  * main - Entry point
  *
- * code prints small letters of the alphabet and then capital letters with a new line
- *
- * return always 0 (success)
+ * Return: always 0 (success)
  */
-int main() {
+
+int main(void)
+	{
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
+
 	putchar(c);
 
 	for (c = 'A'; c <= 'Z'; c++)
-	
+
 	putchar(c);
 	putchar('\n');
-	return 0;
+	return (0);
 }
