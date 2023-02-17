ae79f14463c7c3ff5a15d2199936c30a67a844a3
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-17 07:52:01 -0800

    alphabets_lowercasee

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+	{
+	char alpha[26];
+	alpha = ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,	t,u,v,w,x,y,z");
+		
+	for (i=0;i<26;i++)
+	{
+	putchar(alpha[i]\n);
+	}
+	return 0;
+	}
