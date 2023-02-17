12bb9a3e97ab274f412f72f7687a98c96889a6c9
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-17 06:27:19 -0800

    last_digit_finder

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,37 @@
-#include <stdlib.h> 
- #include <time.h> 
- /* more headers goes there */ 
-  
- /* betty style doc for function main goes there */ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The main function generates a random number
+ * and the variable lastdigit computes the last digit
+ * which is fed to the if statements to output the corr
+ * esponding strings
+ *
+ * return Always 0;
+ *
+ */
  int main(void) 
  { 
          int n; 
   
          srand(time(0)); 
-         n = rand() - RAND_MAX / 2; 
-         /* your code goes there */ 
-         return (0); 
+         n = rand() - RAND_MAX / 2;
+
+         /* your code goes there */
+
+	 int lastdigit = n % 10;
+ if (lastdigit > 5) 
+ {
+ printf("Last digit of %d is %d and is greater than 5",n,lastdigit);
+  }
+ else if (lastdigit == 0)
+ {
+ printf("Last digit of %d is %d and is 0",n,lastdigit);
+ }
+ else (lastdigit < 6 && != 0)
+ {
+ printf("Last digit of %d is %d and is less than 6 and not 0",n,lastdigit);
+ }
+ return (0);
  }
