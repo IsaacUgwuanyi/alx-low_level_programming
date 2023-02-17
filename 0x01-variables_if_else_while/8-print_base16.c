6d63a3444390d90ef289f2f9c1674c300d28c6ac
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-17 14:46:11 -0800

    fixes_betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,22 +9,21 @@
 
 int main(void)
 {
-	    int i;
-	    char c;
+	int i;
+	char c;
 
-	    for (i = 0; i < 16; i++)
-	    {
-	        if (i < 10)
-	        {
-	            c = i + '0';
-	        }
-	        else
-	        {
-	            c = i - 10 + 'a';
-	        }
-	        putchar(c);
-	    }
-	    putchar('\n');
-
-	    return (0);
+	for (i = 0; i < 16; i++)
+	{
+	if (i < 10)
+	{
+	c = i + '0';
+	}
+	else
+	{
+	c = i - 10 + 'a';
+	}
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
