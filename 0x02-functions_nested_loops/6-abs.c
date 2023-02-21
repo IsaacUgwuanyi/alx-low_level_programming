47fd1c8deb07aa8b40ede5943f5bae3c95b55461
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-21 11:15:52 -0800

    checks_for_estimate_of_number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - estimates the absolute value of a number
+ * @k: the number to be checked
+ *
+ * Return: the absolute value or estimate of the 
+ * number
+ */
+
+int _abs(int k)
+{
+	if (k >= 0)
+		return (k);
+	else
+		return (-k);
+}
