e6ce42fca828ec37f5faefae3e1ca9eb819e09a0
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-21 10:47:00 -0800

    checks_for_alphabets

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ int _putchar(char b);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int b);
-int isalpha(int b);
+int _isalpha(int b);
 int print_sign(int k);
 int _abs(int);
 int print_last_digit(int);
