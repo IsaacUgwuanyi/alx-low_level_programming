8b774c73f536c89881564c1d3448e8abbacbe199
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-21 09:54:32 -0800

    alphabets_lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point ,this prints alphabets in 
+ * lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_alphabet(void)
+	{
+		char alpha;
+		for(alpha = 'a'; alpha <= 'z'; alpha++);
+		_putchar(alpha);
+
+		_putchar('\n');
+	}
+	return(0);
+}
