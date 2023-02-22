13b425878497c464f4997c25d6985e0414a1fa6b
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-22 15:42:12 -0800

    all

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if number is posi
+ * tive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
