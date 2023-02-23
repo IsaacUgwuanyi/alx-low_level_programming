ac5e7f3bc3350323ac99e77e683bba890cd18f0c
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-02-23 06:13:58 -0800

    Return_adjustable

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers 0 to 9 while ommitting number 2 and 4
+ * print_most_numbers - prints numbers 0 to 9 while
+ * ommiting 2 and 4
  *
  * Return: void
  */
