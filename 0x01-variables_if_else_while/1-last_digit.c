529b5e6e300b58ed68def069efb7155544b18bc2
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-03-02 08:25:29 -0800

    beautified file to betty standards

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,32 +5,38 @@
 /**
  * main - Entry point of the program
  *
- * Description: The main function generates a random number and
- * computes the last digit to output the corresponding string.
+ * Description: The main function generates a random 
+ * number and
+ * computes the last digit to output the corresponding 
+ * string.
  *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-int n;
-int lastdigit;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
+	int n;
+	int lastdigit;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-lastdigit = n % 10;
+	lastdigit = n % 10;
 
-if (lastdigit > 5)
-{
-printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
-}
-else if (lastdigit == 0)
-{
-printf("Last digit of %d is %d and is 0\n", n, lastdigit);
-}
-else
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
-}
-return (0);
-}
+	if (lastdigit > 5)
+	{
+	printf("Last digit of %d is %d and is greater 
+			than 5\n", n, lastdigit);
+	}
+	else if (lastdigit == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, 
+			lastdigit);
+	}
+	else
+	{
+	printf("Last digit of %d is %d and is less 
+			than 6 and not 0\n", n, 
+			lastdigit);
+	}
+	return (0);
+	}
