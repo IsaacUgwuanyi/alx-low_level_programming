242503201453ef097772ce1fe67454ac9b701075
Isaacugwuanyi <isaacugwuanyi90@gmail.com>
2023-03-11 04:36:04 -0800

    static_file_edit

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,25 +1,18 @@
 #include "main.h"
+/**
+* _puts - prints a string
+* @str: single character type
+*/
 
 void _puts(char *str)
-
-#include<stdio.h>
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (Success)
-
- */
-
-int main(void)
-
 {
+	int i;
 
-        puts("\"Programming is like building a multilingual puzzle");
-
-        return (0);
-
+	i = 0;
+	while (str[i] != 0)
+	{
+	_putchar(str[i]);
+	i++;
+	}
+	_putchar('\n');
 }
