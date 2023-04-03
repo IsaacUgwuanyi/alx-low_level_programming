21cf5be7add97c3f3186e7c1023eb7a2c63f31af
Isaac ugwuanyi <isaacugwuanyi90@gmail.com>
2023-04-03 13:07:06 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,44 +9,35 @@
 *
 * Return: the address of the new node, or NULL if it failed
 */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new_node, *temp = *head;
-unsigned int i;
+	unsigned int i;
+	listint_t *temp, *new;
 
-/* Check if head is NULL */
-if (*head == NULL)
-return (NULL);
-
-/* Create a new node */
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
-
-/* Add the new node at the beginning of the list */
-if (idx == 0)
-{
-new_node->next = *head;
-*head = new_node;
-return (new_node);
+	if (head == NULL)
+		return (NULL);
+	if (idx != 0)
+	{
+		temp = *head;
+		for (i = 0; i < idx - 1 && temp != NULL; i++)
+		{
+			temp = temp->next;
+		}
+		if (temp == NULL)
+			return (NULL);
+	}
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	new->next = temp->next;
+	temp->next = new;
+	return (new);
 }
-
-/* Traverse the list to the given index */
-for (i = 0; i < idx - 1 && temp != NULL; i++)
-temp = temp->next;
-
-/* Check if index is out of range */
-if (temp == NULL)
-{
-free(new_node);
-return (NULL);
-}
-
-/* Insert the new node at the given index */
-new_node->next = temp->next;
-temp->next = new_node;
-
-return (new_node);
-}
-
