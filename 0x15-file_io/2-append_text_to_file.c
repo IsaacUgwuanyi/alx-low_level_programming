16b1f2fe7e6cd23cb48ff8b792b298f1c1460800
Isaac ugwuanyi <isaacugwuanyi90@gmail.com>
2023-04-07 15:29:04 -0700

    copyfile

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * str_length - Finds the length of a string.
- * @str: Pointer to the string.
+ * _strlen - finds the length of a string input
+ * @str: pointer to the string
  *
- * Return: Length of the string.
+ * Return: The length of the string
  */
-size_t str_length(char *str)
+size_t _strlen(char *str)
 {
 	size_t i;
 
@@ -16,27 +16,26 @@ size_t str_length(char *str)
 }
 
 /**
- * append_text_to_file - Appends text to the end of a file.
- * @filename: Name of the file.
- * @text_content: NULL terminated string to append to the file.
+ * append_text_to_file - Adds a text at the end of a file.
+ * @filename: Name of the file
+ * @text_content: string to add at the end of the file
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file_descriptor;
-	ssize_t write_length;
+	int fd;
+	ssize_t len;
 
 	if (filename == NULL)
 		return (-1);
-	file_descriptor = open(filename, O_WRONLY | O_APPEND);
-	if (file_descriptor == -1)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
 		return (-1);
 	if (text_content != NULL)
-		write_length = write(file_descriptor, text_content, str_length(text_content));
-	close(file_descriptor);
-	if (write_length == -1)
+		len = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (len == -1)
 		return (-1);
 	return (1);
 }
-
